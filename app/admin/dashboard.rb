46a120bfba3a3bde4f983fa98386496cf8f70f15
bcoelho44 <bcoelho44@academic.rrc.ca>
2024-11-14 17:01:03 -0600

    Added a logout button with functionality to admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
+
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
+    # Welcome message
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
         span I18n.t("active_admin.dashboard_welcome.welcome")
@@ -10,24 +12,9 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+    # Logout button
+    div class: "logout-button" do
+      link_to "Logout", destroy_admin_user_session_path, method: :delete, class: "button is-danger"
+    end
+  end
 end
